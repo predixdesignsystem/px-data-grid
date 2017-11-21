99d709c64f878f2fc1e1b479ebcea96c17fc3631
Toshegg <Toshegg190494@gmail.com>
2017-11-21 16:18:50 +0200

    Spinner for remote data provider (#55)

--- test/px-data-grid-test.js
+++ test/px-data-grid-test.js
@@ -30,18 +30,54 @@ function runTests() {
 
   describe('Unit Tests for px-data-grid', () => {
     let grid;
+    let timeoutSpy;
 
     describe('simple-grid tests', () => {
+      function createTimeoutSpy() {
+        timeoutSpy = sinon.spy(window, 'setTimeout');
+
+        grid.remoteDataProvider = () => {};
+
+        const call = timeoutSpy.getCalls().filter(call => call.returnValue === grid._spinnerHiddenTimeout)[0];
+        return call.args;
+      }
+
       beforeEach(() => {
         grid = fixture('simple-grid');
         grid.tableData = data;
       });
 
-      it('should properly populate _generatedColumns', function() {
+      it('should properly populate _generatedColumns', () => {
         expect(grid._generatedColumns.length).to.be.eql(3);
         expect(grid._generatedColumns[0].name).to.be.eql('first');
         expect(grid._generatedColumns[1].name).to.be.eql('last');
         expect(grid._generatedColumns[2].name).to.be.eql('email');
+      });
+
+      it('should set _spinnerHiddenTimeout when assigning remoteDataProvider', () => {
+        grid.remoteDataProvider = (params, callback) => {};
+        expect(grid._spinnerHiddenTimeout).to.be.ok;
+      });
+
+      it('should hide spinner unless 500ms have passed', () => {
+        grid.remoteDataProvider = (params, callback) => {};
+        expect(grid.shadowRoot.querySelector('px-spinner').hasAttribute('hidden')).to.be.true;
+      });
+
+      it('should have proper timeout', () => {
+        grid.loadingSpinnerDebounce = 1000;
+        const timeout = createTimeoutSpy()[1];
+
+        expect(timeout).to.equal(1000);
+        timeoutSpy.restore();
+      });
+
+      it('should show spinner after 500ms when data is loading', () => {
+        const timeoutCallback = createTimeoutSpy()[0];
+
+        timeoutCallback();
+        expect(grid.shadowRoot.querySelector('px-spinner').hasAttribute('hidden')).to.be.false;
+        timeoutSpy.restore();
       });
     });
 
@@ -56,7 +92,7 @@ function runTests() {
         });
       });
 
-      it('should set _generatedColumns to empty array', function() {
+      it('should set _generatedColumns to empty array', () => {
         expect(grid._generatedColumns.length).to.be.eql(0);
       });
     });
