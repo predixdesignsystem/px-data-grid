e65f070aa9e20267badf9cfa22d0cf69f903d404
Limon Monte <limon.monte@gmail.com>
2017-12-05 14:18:07 +0200

    Move selection tests to test/selections.js

--- test/selection.js
+++ test/selection.js
@@ -13,14 +13,47 @@ document.addEventListener('WebComponentsReady', () => {
       });
     });
 
-    it('should add the item to selectedItems when row is clicked', (done) => {
-      grid.selectable = true;
-      Polymer.RenderStatus.afterNextRender(grid, () => {
-        const rows = getRows(grid);
-        const cell = getRowCells(rows[1])[0];
-        cell.click();
-        expect(grid.selectedItems).to.eql([tableData[1]]);
-        done();
+    describe('multi selection', () => {
+      it('should add the item to selectedItems when row is clicked', (done) => {
+        grid.selectable = true;
+        grid.multiSelect = true;
+        Polymer.RenderStatus.afterNextRender(grid, () => {
+          expect(grid.selectedItems).to.eql([]);
+          const rows = getRows(grid);
+          let cell = getRowCells(rows[1])[0];
+          cell.click();
+          Polymer.RenderStatus.afterNextRender(grid, () => {
+            expect(grid.selectedItems).to.eql([tableData[1]]);
+            cell = getRowCells(rows[2])[0];
+            cell.click();
+            Polymer.RenderStatus.afterNextRender(grid, () => {
+              expect(grid.selectedItems).to.eql([tableData[1], tableData[2]]);
+              done();
+            });
+          });
+        });
+      });
+    });
+
+    describe('single selection', () => {
+      it('should only have one item in selectedItems when rows have been clicked', (done) => {
+        expect(grid.selectedItems).to.eql([]);
+        grid.selectable = true;
+        grid.multiSelect = false;
+        Polymer.RenderStatus.afterNextRender(grid, () => {
+          const rows = getRows(grid);
+          let cell = getRowCells(rows[1])[1];
+          cell.click();
+          Polymer.RenderStatus.afterNextRender(grid, () => {
+            expect(grid.selectedItems).to.eql([tableData[1]]);
+            cell = getRowCells(rows[2])[1];
+            cell.click();
+            Polymer.RenderStatus.afterNextRender(grid, () => {
+              expect(grid.selectedItems).to.eql([tableData[2]]);
+              done();
+            });
+          });
+        });
       });
     });
   });
