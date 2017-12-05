f0d45468af58f7a890ec81e80ec2ec3a477d6d30
Sami Viitanen <sami.viitanen@vaadin.com>
2017-12-05 14:43:22 -0800

    Fix and improve tests, also use better property name

--- test/highlighting.js
+++ test/highlighting.js
@@ -24,7 +24,8 @@ document.addEventListener('WebComponentsReady', () => {
         }
       }];
 
-      expect(grid._getCellStyle(tableData[2], grid.columns[0])).to.eql('background: red');
+      expect(grid._resolveCellColor(tableData[2], grid.columns[0])).to.eql('red');
+      expect(grid._resolveCellColor(tableData[1], grid.columns[0])).to.eql(undefined);
     });
 
     it('should highlight row', () => {
@@ -36,9 +37,12 @@ document.addEventListener('WebComponentsReady', () => {
         }
       }];
 
-      expect(grid._getCellStyle(tableData[3], grid.columns[0])).to.eq('background: yellow');
-      expect(grid._getCellStyle(tableData[3], grid.columns[1])).to.eq('background: yellow');
-      expect(grid._getCellStyle(tableData[3], grid.columns[2])).to.eq('background: yellow');
+      expect(grid._resolveCellColor(tableData[3], grid.columns[0])).to.eq('yellow');
+      expect(grid._resolveCellColor(tableData[3], grid.columns[1])).to.eq('yellow');
+      expect(grid._resolveCellColor(tableData[3], grid.columns[2])).to.eq('yellow');
+      expect(grid._resolveCellColor(tableData[2], grid.columns[0])).to.eq(undefined);
+      expect(grid._resolveCellColor(tableData[2], grid.columns[1])).to.eq(undefined);
+      expect(grid._resolveCellColor(tableData[2], grid.columns[2])).to.eq(undefined);
     });
 
     it('should highlight column', () => {
@@ -51,8 +55,21 @@ document.addEventListener('WebComponentsReady', () => {
       }];
 
       tableData.forEach((d) => {
-        expect(grid._getCellStyle(d, grid.columns[0])).to.eq('background: blue');
+        expect(grid._resolveCellColor(d, grid.columns[0])).to.eq('blue');
+        expect(grid._resolveCellColor(d, grid.columns[1])).to.eql(undefined);
       });
+    });
+
+    it('should highlight cell with default', () => {
+      grid.highlight = [{
+        type: 'cell',
+        condition: (cellData, column, item) => {
+          return cellData === 'Alma';
+        }
+      }];
+
+      expect(grid._resolveCellColor(tableData[2], grid.columns[0])).to.eql('default');
+      expect(grid._resolveCellColor(tableData[2], grid.columns[1])).to.eql(undefined);
     });
   });
 });
