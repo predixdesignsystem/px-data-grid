ba2853503fe7ddf3a65c377d474421cb9dbe9c16
Sami Viitanen <sami.viitanen@gmail.com>
2017-12-08 05:55:37 -0800

    Force column update API and changed table actions API (#127)
    * Adding updateColumns API to help with column manipulations
    
    * Change table actions API to not have functions
    
    Remove console print left behind

--- test/columns-api.js
+++ test/columns-api.js
@@ -18,12 +18,20 @@ document.addEventListener('WebComponentsReady', () => {
     });
 
     describe('manually passed columns', () => {
-      const columns = [
+      const columnsA = [
         {
           name: 'first',
           header: 'First Name',
           path: 'first'
         },
+        {
+          header: 'Hidden column',
+          hidden: true,
+          path: ''
+        }
+      ];
+
+      const columnsB = [
         {
           name: 'last',
           header: 'Last Name',
@@ -41,11 +49,70 @@ document.addEventListener('WebComponentsReady', () => {
         }
       ];
 
-      it('should change generated column to manually passed', () => {
-        grid.columns = columns;
+      it('should change generated no columns to manually passed', () => {
+
+        // Check that no columns as no data, then set first columns
+        let currentColumns = grid._vaadinGrid.querySelectorAll('px-data-grid-column');
+        expect(currentColumns.length).to.be.eq(0);
+        grid.columns = columnsA;
         flushVaadinGrid(grid);
-        expect(grid._vaadinGrid.querySelectorAll('px-data-grid-column').length).to.be.eq(4);
+
+        // Verify columns set correctly and set new columns
+        currentColumns = grid._vaadinGrid.querySelectorAll('px-data-grid-column');
+        expect(currentColumns.length).to.be.eq(2);
+        expect(currentColumns[0].name).to.be.eq('first');
+        grid.columns = columnsB;
+        flushVaadinGrid(grid);
+
+        // Verify columns set correct and then modify columns and update
+        currentColumns = grid._vaadinGrid.querySelectorAll('px-data-grid-column');
+        expect(currentColumns.length).to.be.eq(3);
+        expect(currentColumns[0].name).to.be.eq('last');
       });
+    });
+
+    it('should be handle column data update with updateColumns', () => {
+      const columnsA = [
+        {
+          name: 'last',
+          header: 'Last Name',
+          path: 'last'
+        },
+        {
+          name: 'email',
+          header: 'Email',
+          path: 'email'
+        },
+        {
+          header: 'Hidden column',
+          hidden: true,
+          path: ''
+        }
+      ];
+
+      // Should not have columns at the start
+      let currentColumns = grid._vaadinGrid.querySelectorAll('px-data-grid-column');
+      expect(currentColumns.length).to.be.eq(0);
+      grid.columns = columnsA;
+      flushVaadinGrid(grid);
+
+      // Modfy data and call updateColumns
+      currentColumns = grid._vaadinGrid.querySelectorAll('px-data-grid-column');
+      expect(currentColumns.length).to.be.eq(columnsA.length);
+      expect(currentColumns[0].name).to.be.eq(columnsA[0].name);
+      expect(currentColumns[1].name).to.be.eq(columnsA[1].name);
+      const modifiedColumns = grid.columns.slice();
+      modifiedColumns[0].hidden = true;
+      modifiedColumns[0].name = 'Modified';
+      grid.columns = modifiedColumns;
+      grid.updateColumns();
+      flushVaadinGrid(grid);
+
+      // Finally verify column modifications
+      currentColumns = grid._vaadinGrid.querySelectorAll('px-data-grid-column');
+      expect(currentColumns.length).to.be.eq(3);
+      expect(currentColumns[0].name).to.be.eq('Modified');
+      expect(currentColumns[0].hidden).to.be.eq(true);
     });
   });
 });
