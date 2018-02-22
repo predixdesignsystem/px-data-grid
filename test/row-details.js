4fa73c082b9dc73a23c84172649af2718181a441
Limon Monte <limon.monte@gmail.com>
2018-02-22 08:53:07 +0200

    Refactor: rowDetails -> _rowDetails, add rowDetails tests (#691)

---
+++ test/row-details.js
@@ -0,0 +1,33 @@
+document.addEventListener('WebComponentsReady', () => {
+  describe('row details', () => {
+    let grid;
+
+    beforeEach((done) => {
+      grid = fixture('px-data-grid-row-details-fixture');
+      grid.tableData = tableData;
+
+      Polymer.RenderStatus.afterNextRender(grid, () => {
+        setTimeout(() => { // IE11
+          done();
+        });
+      });
+    });
+
+    it('should open and close row with setRowDetailsVisible(item)', (done) => {
+      grid.setRowDetailsVisible(grid.tableData[0], true);
+      setTimeout(() => {
+        const rowDetails = getRows(grid)[0].querySelector('td:last-child');
+        expect(rowDetails.hasAttribute('aria-expanded')).to.be.true;
+        expect(rowDetails.hidden).to.be.false;
+
+        grid.setRowDetailsVisible(grid.tableData[0], false);
+        setTimeout(() => {
+          const rowDetails = getRows(grid)[0].querySelector('td:last-child');
+          expect(rowDetails.hidden).to.be.true;
+          done();
+        }, 50);
+      }, 50);
+    });
+
+  });
+});
